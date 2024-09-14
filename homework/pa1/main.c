63580fce79841891cdc43cd24c32b20758d39b37
chabitblunk <cblunk097@gmail.com>
2024-09-13 23:46:29 -0600

    working block and stream cipher, needed to rewrite and add error handling

--- homework/pa1/main.c
+++ homework/pa1/main.c
@@ -4,102 +4,183 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int main(int argc, char *argv[]) {
-  if (argc == 6) {
-    // arg 1: check for block or stream
-    if (strcmp(argv[1], "B") != 0 || strcmp(argv[1], "S") != 0) {
-      printf("invalid function type");
-      return EXIT_FAILURE;
-    }
-    // arg 2: check if input file path leads to a file
-    if (fopen(argv[2], "r") == NULL) {
-      printf("input file does not exist");
-      return EXIT_FAILURE;
-    }
-    // arg 4: check if key file path leads to a file
-    if (fopen(argv[4], "r") == NULL) {
-      printf("key file does not exist");
-    }
-    // arg 5: check for encrypt or decrypt
-    if (strcmp(argv[5], "E") != 0 || strcmp(argv[5], "D") != 0) {
-      printf("invalid mode type");
-      return EXIT_FAILURE;
-    }
+struct message {
+  unsigned size;
+  char *content;
+};
+
+struct message read_message(const char *fname) {
+  struct message out;
+  FILE *fp = fopen(fname, "r");
+  if (fp == NULL) {
+    perror("fopen");
+    exit(EXIT_FAILURE);
   }
-  return EXIT_SUCCESS;
+
+  struct stat st;
+  if (stat(fname, &st) != 0) {
+    perror("stat");
+    exit(EXIT_FAILURE);
+  }
+
+  out.size = st.st_size;
+  out.content = malloc(out.size);
+  if (out.content == NULL) {
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
+
+  fread(out.content, 1, out.size, fp);
+  fclose(fp);
+  return out;
 }
 
-// function to encrypt & decrypt using a stream cipher
-// TODO: this is essentially done. fix main and then test
-char *scencrypt(const char *msgfname, const char *keyfname,
-                const char *outfname) {
-  // msg stats, fp, length, & string
-  // <sys/stat.h> is *the* tool for this
-  struct stat msgst;
-  FILE *msgfp = fopen(msgfname, "r");
-  stat(msgfname, &msgst);
-  unsigned msglen = msgst.st_size;
-  char msg[msglen];
-  fgets(msg, msglen, msgfp);
+int scencrypt(const char *msgfname, const char *keyfname,
+              const char *outfname) {
+  struct message msg = read_message(msgfname);
+  struct message key = read_message(keyfname);
 
-  // key stats, fp, length, & string
-  struct stat keyst;
-  FILE *keyfp = fopen(keyfname, "r");
-  stat(keyfname, &keyst);
-  unsigned keylen = keyst.st_size;
-  char key[keylen];
-  fgets(key, keylen, keyfp);
-
-  // output string
-  char out[msglen];
-
-  // algorithm
-  int i = 0, j = 0;
-  while ((i++) < msglen) {
-    if (j == keylen) {
-      j = 0;
-    }
-    out[i] = msg[i] ^ key[j];
-    j++;
-  }
-
-  // create output fp
-  FILE *outfp = fopen(outfname, "w");
-  if (outfp == NULL) {
+  char *out = malloc(msg.size);
+  if (out == NULL) {
+    perror("malloc");
     return EXIT_FAILURE;
   }
-  // write to out file
-  fputs(out, outfp);
+
+  for (unsigned i = 0, j = 0; i < msg.size; i++) {
+    if (j == key.size) {
+      j = 0;
+    }
+    out[i] = msg.content[i] ^ key.content[j++];
+  }
+
+  FILE *outfp = fopen(outfname, "w");
+  if (outfp == NULL) {
+    perror("fopen");
+    free(out);
+    return EXIT_FAILURE;
+  }
+
+  fwrite(out, 1, msg.size, outfp);
+  fclose(outfp);
+  free(out);
+  free(msg.content);
+  free(key.content);
   return EXIT_SUCCESS;
 }
 
-void padchars(char block[16]) {
-  for (int i = 0; i < 16; i++) {
-    if (block[i] == 0 && block[i + 1] == 0) {
-      block[i] = 8;
-      if (i <= 15) {
-        block[i + 1] = 1;
+void padchars(char *block, unsigned size) {
+  for (unsigned i = 0; i < size; i++) {
+    if (block[i] == 0x0) {
+      block[i] = 0x81;
+    }
+  }
+}
+
+void swapchars(char *block, unsigned size, int reverse) {
+  if (reverse) {
+    for (unsigned i = 0, j = size - 1; i < j; i++, j--) {
+      if (block[j] % 2 == 0) {
+        char temp = block[j];
+        block[j] = block[i];
+        block[i] = temp;
+      }
+    }
+  } else {
+    for (unsigned i = 0, j = size - 1; i < j; i++, j--) {
+      if (block[i] % 2 == 0) {
+        char temp = block[i];
+        block[i] = block[j];
+        block[j] = temp;
       }
     }
   }
 }
 
-// function to encrypt using a block cipher
-int bcencrypt(const char *infname, const char *keyfname) {
-  FILE *keyfp = fopen(keyfname, "r");
-  struct stat keyst;
-  stat(keyfname, &keyst);
-  if (keyst.st_size > 16) {
-    printf("key longer than 16 bytes\n");
+void blockxor(const char *msg, const char *key, char *output, unsigned size) {
+  for (unsigned i = 0; i < size; i++) {
+    output[i] = msg[i] ^ key[i];
+  }
+}
+
+int bcencryptdecrypt(const char *msgfname, const char *keyfname,
+                     const char *outfname, int encrypt) {
+  struct message msg = read_message(msgfname);
+  struct message key = read_message(keyfname);
+  if (key.size < 16) {
+    key.content = realloc(key.content, 16);
+    if (key.content == NULL) {
+      perror("realloc");
+      return EXIT_FAILURE;
+    }
+    padchars(key.content, 16);
+    key.size = 16;
+  }
+  if (key.size > 16) {
+    fprintf(stderr, "block key must be exactly 16 chars\n");
+    free(msg.content);
+    free(key.content);
     return EXIT_FAILURE;
   }
-  char keyblock[16];
-  fgets(keyblock, sizeof(keyblock), keyfp);
-  padchars(keyblock);
-  FILE *infp = fopen(infname, "r");
-  struct stat inst;
-  stat(infname, &inst);
-  if (inst.st_size < 16) {
+
+  FILE *out = fopen(outfname, "w");
+  if (out == NULL) {
+    perror("fopen");
+    free(msg.content);
+    free(key.content);
+    return EXIT_FAILURE;
   }
+
+  char *msgblock = malloc(key.size);
+  char *output = malloc(key.size);
+  if (msgblock == NULL || output == NULL) {
+    perror("malloc");
+    fclose(out);
+    free(msg.content);
+    free(key.content);
+    return EXIT_FAILURE;
+  }
+
+  for (unsigned i = 0; i < msg.size; i += key.size) {
+    memset(msgblock, 0x81, key.size);
+    for (unsigned j = 0; j < key.size && i + j < msg.size; j++) {
+      msgblock[j] = msg.content[i + j];
+    }
+
+    if (encrypt) {
+      blockxor(msgblock, key.content, output, key.size);
+      swapchars(msgblock, key.size, 0);
+    } else {
+      swapchars(output, key.size, 1);
+      blockxor(msgblock, key.content, output, key.size);
+    }
+
+    fwrite(output, 1, key.size, out);
+  }
+
+  fclose(out);
+  free(msgblock);
+  free(output);
+  free(msg.content);
+  free(key.content);
   return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 6) {
+    fprintf(stderr,
+            "usage: %s [B|S] <input_file> <output_file> <key_file> [E|D]\n",
+            argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  if (strcmp(argv[1], "B") == 0) {
+    return bcencryptdecrypt(argv[2], argv[4], argv[3],
+                            strcmp(argv[5], "E") == 0);
+  } else if (strcmp(argv[1], "S") == 0) {
+    return scencrypt(argv[2], argv[4], argv[3]);
+  } else {
+    fprintf(stderr, "invalid mode type\n use 'B' for block cipher or 'S' for "
+                    "stream cipher\n");
+    return EXIT_FAILURE;
+  }
 }
